dd0ab1d9016a86bceed531fa1ec82f13f1d2a05f
John Marc Balbada <kumuro123@gmail.com>
2023-10-03 14:47:34 +0800

    commit message

---
+++ Balbada_LowGraphicsActivity/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Balbada_LowGraphicsActivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.errorMsg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // errorMsg
+            // 
+            this.errorMsg.AutoSize = true;
+            this.errorMsg.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorMsg.ForeColor = System.Drawing.Color.Red;
+            this.errorMsg.Location = new System.Drawing.Point(242, 429);
+            this.errorMsg.Name = "errorMsg";
+            this.errorMsg.Size = new System.Drawing.Size(22, 14);
+            this.errorMsg.TabIndex = 0;
+            this.errorMsg.Text = "     ";
+            this.errorMsg.Click += new System.EventHandler(this.errorMsg_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(21, 430);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "W -";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(65, 430);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "A - ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(112, 430);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "S -";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(162, 430);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "D - ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(47, 433);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 9);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "UP";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(87, 433);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 9);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "LEFT";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(132, 433);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 9);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "DOWN";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(184, 433);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 9);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "RIGHT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.errorMsg);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "s";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label errorMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Balbada_LowGraphicsActivity/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Balbada_LowGraphicsActivity
+{
+
+    public partial class Form1 : Form
+    {
+        Rectangle r;
+        Graphics gr;
+
+        int formWidth;
+        int formHeight;
+
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formWidth = this.Width;
+            formHeight = this.Height;
+            r = new Rectangle(50, 50, 50, 1);
+            Pen newPen = new Pen(Color.White, 1);
+            gr = this.CreateGraphics();
+
+            gr.DrawEllipse(newPen, r);
+
+            t.Interval = 1;
+            t.Tick += T_Tick;
+            t.Start();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                gr.DrawRectangle(new Pen(Brushes.White, 6), r);
+            }catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private void clearRectangle()
+        {
+            //gr.Clear(Color.Gray);
+            this.Invalidate();
+        }
+
+        private void checkBoundary()
+        {
+
+        }
+
+        private void rightMove()
+        {
+            r.X += 5;
+        }
+
+        private void leftMove()
+        {
+            r.X -= 5;
+        }
+
+        private void upMove()
+        {
+            r.Y -= 5;
+        }
+
+        private void downMove()
+        {
+            r.Y += 5;
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char pressed = Char.ToLower(e.KeyChar);
+            switch(pressed)
+            {
+                case 'a':
+                    {
+                        //move to left <- (-X)
+                        int newLeft = r.Left - 10;
+
+                        if (newLeft >= 0)
+                        {
+                            if (r.Height != 1)
+                            {
+                                int temp = r.Width;
+                                r.Width = r.Height;
+                                r.Height = temp;
+                            }
+                            this.BackColor = Color.Gray;
+                            clearRectangle();
+                            leftMove();
+                            Console.WriteLine("left = " + r.Left);
+                            errorMsg.Text = "";
+                        }
+                        else
+                        {
+                            this.BackColor = Color.Black;
+                            errorMsg_Click(sender, e);
+                        }
+                    }
+                    break;
+                case 'w':
+                    // positive y (+Y)
+                    {
+                        int newUp = r.Top - 6;
+
+                        if (newUp >= 0)
+                        {
+                            if(r.Height == 1)
+                            {
+                                int temp = r.Width;
+                                r.Width = r.Height;
+                                r.Height = temp;
+                            }
+                            this.BackColor = Color.Gray;
+                            clearRectangle();
+                            upMove();
+                            Console.WriteLine("up = " + r.Top);
+                            errorMsg.Text = "";
+                        }
+                        else
+                        {
+                            this.BackColor = Color.Black;
+                            errorMsg_Click(sender, e);
+                        }
+                    }
+                    break;
+                case 's':
+                    // negative y (-Y)
+                    {
+                        int newDown = r.Bottom + 10;
+
+                        if (newDown <= 460)
+                        {
+                            if (r.Height == 1)
+                            {
+                                int temp = r.Width;
+                                r.Width = r.Height;
+                                r.Height = temp;
+                            }
+                            this.BackColor = Color.Gray;
+                            clearRectangle();
+                            downMove();
+                            Console.WriteLine("down = " + r.Top + " formHeight = " + formHeight);
+                            errorMsg.Text = "";
+                        }
+                        else
+                        {
+                            this.BackColor = Color.Black;
+                            errorMsg_Click(sender, e);
+                        }
+                    }
+
+                    break;
+                case 'd':
+                    // positive X -> (+X)
+                    {
+                        int newRight = r.Right + 25;
+
+                        if (newRight <= formWidth)
+                        {
+                            if (r.Height != 1)
+                            {
+                                int temp = r.Width;
+                                r.Width = r.Height;
+                                r.Height = temp;
+                            }
+                            this.BackColor = Color.Gray;
+                            clearRectangle();
+                            rightMove();
+                            Console.WriteLine("right = " + r.Right);
+                            errorMsg.Text = "";
+                        }
+                        else
+                        {
+                            this.BackColor = Color.Black;
+                            errorMsg_Click(sender, e);
+                        }
+                    }
+                    break;
+            }
+            
+            
+        }
+
+
+        private void errorMsg_Click(object sender, EventArgs e)
+        {
+            errorMsg.Text = "Error: object is out of range";
+        }
+
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
